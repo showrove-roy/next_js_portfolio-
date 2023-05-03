49671290ceb8d83895c30779b68ee2f6cd092cde
Showrove Roy <showrove200@gmail.com>
2023-05-04 01:10:38 +0600

    About me added

--- src/app/Components/About/About.js
+++ src/app/Components/About/About.js
@@ -8,30 +8,37 @@ export default function About() {
           <div className='flex-1 mx-5 font-sans text-white lg:mb-0 aboutText'>
             <h4 className='my-5 text-3xl font-bold lg:5xl'>About Me</h4>
             <p className='font-light leading-relaxed tracking-wide opacity-75 '>
-              This is Raqib Nur, a Web Application Developer residing in Dhaka,
-              Bangladesh. Even if it sounds humorous, I went to Madrasa and
-              returned as a programmer! ✌<br />
-              <br />I consider myself to be completely blessed as I get to
-              inspect and build websites for a living. Of my intense interest in
-              the Web Platform &amp; SaaS products, I plan to build
-              results-driven digital products for my clients while becoming part
-              of meaningful businesses along the way.
-              <br />
-              <br />I{"'"}ve built numerous websites and worked closely with the
-              founders of the companies. Besides, I{"'"}ve also committed my
-              time to 3 companies part-time to help them hire and manage their
-              own technical team, and I am here today to become a part of your
-              journey.
+              This is Showrove Roy, a skilled MERN stack web developer. With his
+              in-depth knowledge of the Web Platform & SaaS products, Showrove
+              is committed to building high-quality digital products that drive
+              results for his clients. ✌
               <br />
               <br />
-              Recently, I{"'"}ve co-founded an eCommerce Development Company:
-              The Run Digital, where we help entrepreneurs fix their web goals
-              and help them achieve them with our web-based solutions. We are,
-              after all, KP-Idriven.
+              As a MERN stack web developer, I specialize in building dynamic,
+              modern web applications using <strong>MongoDB</strong>,{" "}
+              <strong>Express</strong>, <strong>React</strong>,
+              <strong>Next Js</strong> and ,<strong>Node.js</strong>. With a
+              strong foundation in <strong>JavaScript</strong>, I am skilled at
+              developing both the front-end and back-end of web applications,
+              making me a valuable asset in the development process from start
+              to finish. My experience with the MERN stack allows me to create
+              fast, responsive, and scalable applications that deliver a
+              seamless user experience.
+              <br />
+              <br />
+              In my spare time, I love to explore new technology and stay up to
+              date on the latest industry trends. I also have a passion for
+              traveling and experiencing different cultures. I am always looking
+              for new challenges and opportunities to grow as a developer.
             </p>
           </div>
           <div className=''>
-            <Image src='/raqib-webappdeveloper.webp' width={380} height={425} />
+            <Image
+              src='/raqib-webappdeveloper.webp'
+              width={380}
+              height={425}
+              alt='A man typing laptop'
+            />
           </div>
         </div>
       </div>
